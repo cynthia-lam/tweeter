6ca226cf35c46581edec4ec4c2c5993455ea1218
Cynthia <cynthia-lam@outlook.com>
2024-03-18 00:13:12 -0400

    Add event listener for text area

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,4 +2,14 @@ console.log("Char-counter script is running!");
 
 $(document).ready(function() {
   console.log("DOM is ready!");
+
+  const textarea = document.querySelector("#tweet-text");
+  console.log(textarea);
+
+  textarea.addEventListener("input", () => {
+    console.log("You inputted text into the textarea");
+  });
+
 });
+
+
