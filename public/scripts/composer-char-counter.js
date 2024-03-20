56197db6520f6c40be89c097aa7e23af70533abd
Cynthia <cynthia-lam@outlook.com>
2024-03-19 22:34:49 -0400

    Make the colour of counter turn red if below 0 and back to grey when above 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,6 +9,12 @@ $(document).ready(function() {
   textarea.on("input", function() {
     const textLength = 140 - $(this).val().length;
     $(".counter").text(textLength);
+
+    if (textLength < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "#545149");
+    }
   });
 });
 
